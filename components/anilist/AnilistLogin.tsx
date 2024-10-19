9235dec71a8a715a3d6c400e5a4b544c95f821e0
Tanveer Najib <tanveernajib@gmail.com>
2024-10-19 09:46:53 +0100

    Wording change

--- components/anilist/AnilistLogin.tsx
+++ components/anilist/AnilistLogin.tsx
@@ -13,7 +13,7 @@ export function AnilistLogin({ clientId }: Props) {
     <FullscreenCenter>
       <div className="flex flex-col items-center text-center rounded-lg border-2 border-foreground m-4 p-16">
         <Logo width={200} height={150} className="ml-4" />
-        <h1 className="text-2xl font-bold p-8">Welcome to Tsuki!</h1>
+        <h1 className="text-2xl font-bold p-8">Login with Anilist</h1>
         <a
           className="bg-[#3db4f2] flex gap-2 whitespace-nowrap items-center rounded px-4 py-1 font-bold hover:brightness-90"
           href={`https://anilist.co/api/v2/oauth/authorize?client_id=${clientId}&response_type=token`}
